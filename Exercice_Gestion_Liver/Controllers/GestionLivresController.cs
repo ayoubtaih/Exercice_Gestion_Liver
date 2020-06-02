5e67f6fabaa4e9862adf86f4a1572decec4d171f
ayoub taih <ayoub.taih@gmail.com>
2020-06-02 12:50:16 +0100

    Ajoutez des fichiers projet.

---
+++ Exercice_Gestion_Liver/Controllers/GestionLivresController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Data.Entity;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Exercice_Gestion_Liver.Controllers
+{
+    public class GestionLivresController : Controller
+    {
+        private GestionLiverEntities db = new GestionLiverEntities();
+        // GET: GestionLivres
+        public ActionResult Index()
+        {
+            var ListeLiver = db.Liver.ToList();
+            return View(ListeLiver);
+        }
+        
+        public ActionResult CreerLivre()
+        {
+            
+            return View();
+        }
+        //CreerLivre
+        [HttpPost]
+        public ActionResult CreerLivre( Liver liver)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Liver.Add(liver);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        //DetailsLivre
+        public ActionResult DetailsLivre(int? id)
+        {
+            Liver liver = db.Liver.Find(id);
+            if (liver==null)
+            {
+                return HttpNotFound();
+            }
+            return View(liver);
+        }
+
+
+        public ActionResult EditLivre(int? id)
+        {
+            if (id == null)
+            {
+                return new  HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Liver liver = db.Liver.Find(id);
+            if (liver == null)
+            {
+                return HttpNotFound();
+            }
+            return View(liver);
+        }
+
+        [HttpPost]
+        
+        public ActionResult Edit(Liver liver)
+        {
+
+            if (ModelState.IsValid)
+            {
+                db.Entry(liver).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(liver);
+        }
+
+        public ActionResult SupprimerLivre(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Liver liver = db.Liver.Find(id);
+            if (liver == null)
+            {
+                return HttpNotFound();
+            }
+            return View(liver);
+        }
+
+        [HttpPost,ActionName("SupprimerLivre ")]
+
+        public ActionResult SuppressionConfirmee(int id)
+        {
+
+            Liver liver = db.Liver.Find(id);
+            db.Liver.Remove(liver);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Exercice_Gestion_Liver/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Exercice_Gestion_Liver.MvcApplication" Language="C#" %>
